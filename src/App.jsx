1c4497bf1f0486fd3a9868b8bf192f4d13ba668f
ZYPER-app <jaimecarabias@gmail.com>
2025-05-23 19:48:39 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <main className="min-h-screen p-4 flex flex-col items-center justify-center bg-white text-black">
+      <h1 className="text-3xl font-bold mb-4">ZYPER App</h1>
+      <p>Tu app está funcionando. 🎉 Ahora puedes empezar a personalizarla.</p>
+    </main>
+  );
+}
+
+export default App;
