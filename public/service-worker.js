78116775b055d7370d3434e334354ec8981962d1
ZYPER-app <jaimecarabias@gmail.com>
2025-05-23 18:34:25 +0200

    Add files via upload

---
+++ public/service-worker.js
@@ -0,0 +1,9 @@
+self.addEventListener("install", e => {
+  console.log("Service Worker instalado");
+  e.waitUntil(self.skipWaiting());
+});
+self.addEventListener("activate", e => {
+  console.log("Service Worker activado");
+  return self.clients.claim();
+});
+self.addEventListener("fetch", event => {});
